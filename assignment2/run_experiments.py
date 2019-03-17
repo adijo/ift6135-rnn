f8b7147deb2d2e3781f4f5580c8b7a462dcca7a3
Helgi Tomas Gislason <helgso@gmail.com>
2019-03-17 08:49:19 -0400

    Making note of which models have been run so far for 4.1 and 4.2 in run_experiments.py, which ones are in progress and which ones are left

--- assignment2/run_experiments.py
+++ assignment2/run_experiments.py
@@ -2,21 +2,37 @@ import subprocess
 
 
 def main():
-    # Problems 4.1 and 4.2
-    # Transformer currently does not work, hence commented out
-    commands = [
+    problems_4_1_and_4_2()
+
+
+def problems_4_1_and_4_2():
+    # These are done (both from problem 4.1). We are currently missing the Transformer one (we get NaN in training)
+    # These can be found on https://drive.google.com/drive/folders/1CeaePSAqsOERrAY6zxIqkVKJm75TyB1q?usp=sharing
+    # (they were too big for git to handle)
+    done_commands = [
         "python ptb-lm.py --model=RNN --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_best --save_dir=experiments_problem4_1",
-        "python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_best --save_dir=experiments_problem4_1",
-        # "python ptb-lm.py --model=TRANSFORMER --optimizer=SGD_LR_SCHEDULE --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=0.9 --save_best --save_dir=experiments_problem4_1",
-        "python ptb-lm.py --model=RNN --optimizer=SGD --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_2",
-        "python ptb-lm.py --model=GRU --optimizer=SGD --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_2",
-        # "python ptb-lm.py  --model=TRANSFORMER --optimizer=SGD --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9 --save_dir=experiments_problem4_2",
-        "python ptb-lm.py --model=RNN --optimizer=SGD_LR_SCHEDULE --initial_lr=1 --batch_size=20 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_3",
-        "python ptb-lm.py --model=GRU --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_5",
-        # "python ptb-lm.py  --model=TRANSFORMER --optimizer=ADAM --initial_lr=0.001 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=.9 --save_dir=experiments/problem4_1",
+        "python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_best --save_dir=experiments_problem4_1"
     ]
 
-    for command in commands:
+    commands_in_progress_by_helgi = [
+        "python ptb-lm.py --model=RNN --optimizer=SGD --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_2",
+        "python ptb-lm.py --model=RNN --optimizer=SGD_LR_SCHEDULE --initial_lr=1 --batch_size=20 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_2"
+    ]
+
+    # Leftover experiments for problems 4.1 and 4.2
+    # Transformer currently does not work (we get NaNs when training), hence commented out
+    leftover_commands = [
+        # "python ptb-lm.py --model=TRANSFORMER --optimizer=SGD_LR_SCHEDULE --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=0.9 --save_best --save_dir=experiments_problem4_1",
+        "python ptb-lm.py --model=GRU --optimizer=SGD --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_2",
+        # "python ptb-lm.py  --model=TRANSFORMER --optimizer=SGD --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9 --save_dir=experiments_problem4_2",
+        "python ptb-lm.py --model=GRU --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --save_dir=experiments_problem4_2",
+        # "python ptb-lm.py  --model=TRANSFORMER --optimizer=ADAM --initial_lr=0.001 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=.9 --save_dir=experiments/problem4_2",
+    ]
+
+    # Place the commands in this array you want to run (they will be run one after another, not in parallel)
+    commands_to_run = []
+
+    for command in commands_to_run:
         for message in run_command(command):
             print(message, end="")
 
