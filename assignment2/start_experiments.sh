abfc3ae37f62291ed63200669d12a6f1d9f6f99a
Raphael Jean <raphael.jean@rocketmail.com>
2019-03-18 08:29:36 -0400

    GRU with ADAM curve

---
+++ assignment2/start_experiments.sh
@@ -0,0 +1,10 @@
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=2 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=0.5 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=5 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.7
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=5 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.5
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=5 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.35
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD_LR_SCHEDULE --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=0.9 --save_best
+python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9
+python ptb-lm.py --model=TRANSFORMER --optimizer=ADAM --initial_lr=0.001 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=.9
+
+
