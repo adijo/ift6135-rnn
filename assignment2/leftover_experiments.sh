60d82bf38448462d1aedaca997525140b6884044
Aditya Joshi <1adityajoshi@gmail.com>
2019-03-24 14:42:52 -0400

    Add script to plot loss and grads

--- assignment2/leftover_experiments.sh
+++ assignment2/leftover_experiments.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 python ptb-lm.py --model=TRANSFORMER --optimizer=SGD_LR_SCHEDULE --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=0.9 --save_best --save_dir=experiments_problem4_1
 python ptb-lm.py --model=TRANSFORMER --optimizer=SGD --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9 --save_dir=experiments_problem4_2
 python ptb-lm.py --model=TRANSFORMER --optimizer=ADAM --initial_lr=0.001 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=.9 --save_dir=experiments/problem4_2
---
+++ assignment2/valid_loss_plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('seaborn-whitegrid')
+
+
+def main():
+    rnn_loss = np.load("time_seq_losses_data/rnn_loss.npy")
+    gru_loss = np.load("time_seq_losses_data/gru_loss.npy")
+    trans_loss = np.load("time_seq_losses_data/trans_loss.npy")
+    plt.plot(rnn_loss, marker="o", label="rnn", linestyle="--", alpha=0.5, color="#001f3f")
+    plt.plot(gru_loss, marker="o", label="gru", linestyle="--", alpha=0.5, color="#85144b")
+    plt.plot(trans_loss, marker="o", label="transformer", linestyle="--", alpha=0.5, color="#3D9970")
+    plt.legend()
+    plt.xlabel("Time step (t)")
+    plt.ylabel("Avg valid loss per time step")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
