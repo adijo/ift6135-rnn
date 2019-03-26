88ebcd621cea4042f5ada688f2452ce25d02b761
Aditya Joshi <1adityajoshi@gmail.com>
2019-03-25 20:55:33 -0400

    Change graph colour

--- assignment2/grad_plot.py
+++ assignment2/grad_plot.py
@@ -7,7 +7,7 @@ def main():
     rnn_grad = np.load("grad_plot_data/rnn_grad.npy")
     gru_grad = np.load("grad_plot_data/gru_grad.npy")
     plt.plot(rnn_grad, label="rnn", linestyle="--", alpha=0.5, color="#001f3f")
-    plt.plot(gru_grad, label="gru", linestyle="--", alpha=0.5, color="#85144b")
+    plt.plot(gru_grad, label="gru", linestyle="--", alpha=0.5, color="#FF4136")
     plt.legend()
     plt.xlabel("Time step (t)")
     plt.ylabel("Norm of grad of hidden layers wrt loss of last time step")
--- assignment2/valid_loss_plot.py
+++ assignment2/valid_loss_plot.py
@@ -8,7 +8,7 @@ def main():
     gru_loss = np.load("time_seq_losses_data/gru_loss.npy")
     trans_loss = np.load("time_seq_losses_data/trans_loss.npy")
     plt.plot(rnn_loss, marker="o", label="rnn", linestyle="--", alpha=0.5, color="#001f3f")
-    plt.plot(gru_loss, marker="o", label="gru", linestyle="--", alpha=0.5, color="#85144b")
+    plt.plot(gru_loss, marker="o", label="gru", linestyle="--", alpha=0.5, color="#FF4136")
     plt.plot(trans_loss, marker="o", label="transformer", linestyle="--", alpha=0.5, color="#3D9970")
     plt.legend()
     plt.xlabel("Time step (t)")
